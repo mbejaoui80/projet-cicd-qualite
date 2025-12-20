cf1933cc9517fac558be4936fabe2b07e5c1cb94
mbejaoui80 <testautomationmbe@gmail.com>
2025-12-20 11:42:44 +0100

    Test de detection d'erreur de style

--- test_calcul.py
+++ test_calcul.py
@@ -1,6 +1,13 @@
-def addition(a, b):
-    return a + b
+# def addition(a, b):
+#     return a + b
 
 
-def test_addition():
-    assert addition(10, 5) == 15
+# def test_addition():
+#     assert addition(10, 5) == 15
+
+
+import os  # Import inutile (interdit par les règles strictes)
+
+def addition(a,b): # Pas d'espace après la virgule (C'est moche !)
+    x= 10          # Variable inutile
+    return a+b     # Pas d'espace autour du plus
