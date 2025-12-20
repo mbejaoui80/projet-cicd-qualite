de8a3103199524ab64ca7505eb7659ebf358826e
mbejaoui80 <testautomationmbe@gmail.com>
2025-12-20 12:14:55 +0100

    Tentative de sabotage : Code fonctionnel mais moche

--- test_calcul.py
+++ test_calcul.py
@@ -1,13 +1,8 @@
-# def addition(a, b):
-#     return a + b
+import os#Import collé et inutile
 
+def addition(a,b):#Pas d'espace après la virgule
+    x=10 # Pas d'espace autour du égal et variable inutile
+    return a+b#Pas d'espace autour du plus
 
-# def test_addition():
-#     assert addition(10, 5) == 15
-
-
-import os  # Import inutile (interdit par les règles strictes)
-
-def addition(a,b): # Pas d'espace après la virgule (C'est moche !)
-    x= 10          # Variable inutile
-    return a+b     # Pas d'espace autour du plus
+def test_addition():
+    assert addition(10,5)==15
